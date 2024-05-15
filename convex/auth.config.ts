8ce2c364e2c4d09f84b574124ef5367fd3bbb3e3
pramod <pramodsomanks@gmail.com>
2024-05-16 04:33:16 +0530

    2-Webhooks added

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://upward-leech-37.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
